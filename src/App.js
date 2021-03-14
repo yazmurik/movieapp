4c3df900323d623d48b84f343795febab41a2bc7
yazmurat <yazmurat22@gmail.com>
2021-03-14 13:35:19 -0400

    Created Header and Footer components and styled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import {Link, Route } from 'react-router-dom'
+import {Route } from 'react-router-dom'
 import MoviesPage from './components/pages/MoviesPage';
-function App() {
-  return (
-    <div className="App">
-      App components
-      <br/>
-      <Link to="movies">Movies</Link>
-      <Route path="/movies" component={MoviesPage}></Route>
-    </div>
-  );
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import React, { Component } from 'react'
+import {Container} from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container text>
+          App components
+          <br />
+          <Route path="/movies" component={MoviesPage}></Route>
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
 }
 
+
 export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {Link, Route } from 'react-router-dom';
+import {Container,Image,Menu,Visibility,} from 'semantic-ui-react';
+import {menuStyle, fixedMenuStyle} from '../helpers/styleHelpers'
+
+export default class Header extends Component {
+  state = {
+    menuFixed: false,
+    overlayFixed: false,
+  }
+  stickOverlay = () => this.setState({ overlayFixed: true })
+
+  stickTopMenu = () => this.setState({ menuFixed: true })
+
+  unStickOverlay = () => this.setState({ overlayFixed: false })
+
+  unStickTopMenu = () => this.setState({ menuFixed: false })
+  render(){
+    const { menuFixed} = this.state
+    return (
+    <div>
+      <Visibility
+          onBottomPassed={this.stickTopMenu}
+          onBottomVisible={this.unStickTopMenu}
+          once={false}
+        >
+          <Menu
+            borderless
+            fixed={menuFixed ? 'top' : undefined}
+            style={menuFixed ? fixedMenuStyle : menuStyle}
+          >
+            <Container text>
+              <Menu.Item>
+                <Image size='mini' src='https://react.semantic-ui.com/logo.png' />
+              </Menu.Item>
+              <Menu.Item header>MovieApp</Menu.Item>
+              <Menu.Item as={Link} to="movies">Movies</Menu.Item>
+              {/* <Link to="movies">Movies</Link> */}
+              <Menu.Item as={Link} to="#">Add Movie</Menu.Item>
+            </Container>
+          </Menu>
+        </Visibility>
+    </div>
+  )
+  }
+  
+}
