6e35cde49423099e65a74e15ee28d4778d40119a
yazmurat <yazmurat22@gmail.com>
2021-03-27 17:11:39 -0400

    Styled card edit and delete buttons

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,25 @@
 import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import {Link} from "react-router-dom";
+
+const processButtons=({movieId})=>{
+  return (
+    <Button.Group>
+      <Button animated as={Link} to ={`/movie/${movieId}`}>
+      <Button.Content visible>Edit</Button.Content>
+      <Button.Content hidden>
+        <Icon name='edit' />
+      </Button.Content>
+    </Button>
+    <Button animated='vertical'>
+      <Button.Content visible>Delete</Button.Content>
+      <Button.Content hidden>
+        <Icon name='trash' />
+      </Button.Content>
+    </Button>
+    </Button.Group>
+  )
+}
 
 const MovieCard = ({movie}) => (
   <Card>
@@ -18,6 +38,9 @@ const MovieCard = ({movie}) => (
         <Icon name='user' />
         {movie.imdb_score}
       </a>
+    </Card.Content>
+    <Card.Content extra>
+      {processButtons(movie.id)}
     </Card.Content>
   </Card>
 )
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MovieCard from "../components/MovieCard";
-import { Grid, Image } from "semantic-ui-react";
+import { Grid, Image, } from "semantic-ui-react";
 
 function MovieList({ movies }) {
   const movieList = (
