acace424d4069954853a95a137fa53d89f4feded
yazmurat <yazmurat22@gmail.com>
2021-03-29 14:32:51 -0400

    Ceated home page component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Container,Divider,Grid,Header,Image,List,Segment} from 'semantic-ui-react';
+import {Container,Divider,Grid,Header,Image,Visibility,List,Segment} from 'semantic-ui-react';
+import {Link, NavLink, Route } from 'react-router-dom';
 
 
 function Footer(props) {
@@ -7,59 +8,27 @@ function Footer(props) {
     <div>
       <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
           <Container textAlign='center'>
-            <Grid columns={4} divided stackable inverted>
+            <Grid columns={2} divided stackable inverted>
               <Grid.Row>
                 <Grid.Column>
                   <Header inverted as='h4' content='Group 1' />
                   <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
+                    <List.Item as={Link} to="/">Home</List.Item>
+                    <List.Item as={Link} to="/movies">Movies</List.Item>
+                    <List.Item as={Link} to="/movies/new">Add Movie</List.Item>
                   </List>
                 </Grid.Column>
                 <Grid.Column>
-                  <Header inverted as='h4' content='Group 2' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 3' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Footer Header' />
+                  <Header inverted as='h4' content='Movie INC' />
                   <p>
-                    Extra space for a call to action inside the footer that could help re-engage
-                    users.
+                    ADDRESS: 123 Main street NY Brooklyn
                   </p>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
             <Divider inverted section />
-            <Image src='https://react.semantic-ui.com/logo.png' centered size='mini' />
             <List horizontal inverted divided link size='small'>
-              <List.Item as='a' href='#'>
-                Site Map
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Contact Us
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Terms and Conditions
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Privacy Policy
-              </List.Item>
+              <p>&copy;{new Date().getFullYear()} MOVIEApp INC | All rights reserved | Terms Of Service | Privacy.</p>
             </List>
           </Container>
         </Segment>
